5859b2ebd946daed658f68dd009332ad6585eecb
Jack <jacktvicari@gmail.com>
2022-07-08 11:44:19 -0400

    resized image

---
+++ Assets/scripts/FlowerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBehavior : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator.name);
+        animator.SetBool("isHere", true);
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
